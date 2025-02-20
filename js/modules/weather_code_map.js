7d4bf2d9a42ab781bd1236018b0e73ff6af6dda1
Lucas Norman <lucas.norman007@gmail.com>
2025-02-20 09:33:58 +0100

    Fixed a small issue with the weather icon paths

--- js/modules/weather_code_map.js
+++ js/modules/weather_code_map.js
@@ -3,201 +3,201 @@ export default function () {
         0: {
             day: {
                 description: "Sunny",
-                icon: "../../assets/icons/weather/sunny.svg"
+                icon: "/assets/icons/weather/sunny.svg"
             },
             night: {
                 description: "Clear",
-                icon: "../../assets/icons/weather/clear-night.svg"
+                icon: "/assets/icons/weather/clear-night.svg"
             }
         },
         1: {
             day: {
                 description: "Mainly Sunny",
-                icon: "../../assets/icons/weather/mainly_sunny.svg"
+                icon: "/assets/icons/weather/mainly_sunny.svg"
             },
             night: {
                 description: "Mainly clear",
-                icon: "../../assets/icons/weather/mainly_clear-night.svg"
+                icon: "/assets/icons/weather/mainly_clear-night.svg"
             }
         },
         2: {
             day: {
                 description: "Partly Cloudy",
-                icon: "../../assets/icons/weather/partly_cloudy.svg"
+                icon: "/assets/icons/weather/partly_cloudy.svg"
             },
             night: {
                 description: "Partly Cloudy",
-                icon: "../../assets/icons/weather/partly_cloudy-night.svg"
+                icon: "/assets/icons/weather/partly_cloudy-night.svg"
             }
         },
         3: {
             day: {
                 description: "Cloudy",
-                icon: "../../assets/icons/weather/cloudy.svg"
+                icon: "/assets/icons/weather/cloudy.svg"
             }
         },
         45: {
             day: {
                 description: "Foggy",
-                icon: "../../assets/icons/weather/foggy.svg"
+                icon: "/assets/icons/weather/foggy.svg"
             }
         },
         48: {
             day: {
                 description: "Rime Fog",
-                icon: "../../assets/icons/weather/rime_fog.svg"
+                icon: "/assets/icons/weather/rime_fog.svg"
             }
         },
         51: {
             day: {
                 description: "Light Drizzle",
-                icon: "../../assets/icons/weather/light_drizzle.svg"
+                icon: "/assets/icons/weather/light_drizzle.svg"
             }
         },
         53: {
             day: {
                 description: "Drizzle",
-                icon: "../../assets/icons/weather/drizzle.svg"
+                icon: "/assets/icons/weather/drizzle.svg"
             }
         },
         55: {
             day: {
                 description: "Heavy Drizzle",
-                icon: "../../assets/icons/weather/heavy_drizzle.svg"
+                icon: "/assets/icons/weather/heavy_drizzle.svg"
             }
         },
         56: {
             day: {
                 description: "Light Freezing Drizzle",
-                icon: "../../assets/icons/weather/light_freezing_drizzle.svg"
+                icon: "/assets/icons/weather/light_freezing_drizzle.svg"
             }
         },
         57: {
             day: {
                 description: "Freezing Drizzle",
-                icon: "../../assets/icons/weather/freezing_drizzle.svg"
+                icon: "/assets/icons/weather/freezing_drizzle.svg"
             }
         },
         61: {
             day: {
                 description: "Light Rain",
-                icon: "../../assets/icons/weather/light_rain.svg"
+                icon: "/assets/icons/weather/light_rain.svg"
             }
         },
         63: {
             day: {
                 description: "Rain",
-                icon: "../../assets/icons/weather/rain.svg"
+                icon: "/assets/icons/weather/rain.svg"
             }
         },
         65: {
             day: {
                 description: "Heavy Rain",
-                icon: "../../assets/icons/weather/heavy_rain.svg"
+                icon: "/assets/icons/weather/heavy_rain.svg"
             }
         },
         66: {
             day: {
                 description: "Light Freezing Rain",
-                icon: "../../assets/icons/weather/light_freezing_rain.svg"
+                icon: "/assets/icons/weather/light_freezing_rain.svg"
             }
         },
         67: {
             day: {
                 description: "Freezing Rain",
-                icon: "../../assets/icons/weather/freezing_rain.svg"
+                icon: "/assets/icons/weather/freezing_rain.svg"
             }
         },
         71: {
             day: {
                 description: "Light Snow",
-                icon: "../../assets/icons/weather/light_snow.svg"
+                icon: "/assets/icons/weather/light_snow.svg"
             }
         },
         73: {
             day: {
                 description: "Snow",
-                icon: "../../assets/icons/weather/snow.svg"
+                icon: "/assets/icons/weather/snow.svg"
             }
         },
         75: {
             day: {
                 description: "Heavy Snow",
-                icon: "../../assets/icons/weather/heavy_snow.svg"
+                icon: "/assets/icons/weather/heavy_snow.svg"
             }
         },
         77: {
             day: {
                 description: "Snow Grains",
-                icon: "../../assets/icons/weather/snow_grains.svg"
+                icon: "/assets/icons/weather/snow_grains.svg"
             }
         },
         80: {
             day: {
                 description: "Light Showers",
-                icon: "../../assets/icons/weather/light_showers.svg"
+                icon: "/assets/icons/weather/light_showers.svg"
             },
             night: {
                 description: "Light Showers",
-                icon: "../../assets/icons/weather/light_showers-night.svg"
+                icon: "/assets/icons/weather/light_showers-night.svg"
             }
         },
         81: {
             day: {
                 description: "Showers",
-                icon: "../../assets/icons/weather/showers.svg"
+                icon: "/assets/icons/weather/showers.svg"
             },
             night: {
                 description: "Showers",
-                icon: "../../assets/icons/weather/showers-night.svg"
+                icon: "/assets/icons/weather/showers-night.svg"
             }
         },
         82: {
             day: {
                 description: "Heavy Showers",
-                icon: "../../assets/icons/weather/heavy_showers.svg"
+                icon: "/assets/icons/weather/heavy_showers.svg"
             },
             night: {
                 description: "Heavy Showers",
-                icon: "../../assets/icons/weather/heavy_showers-night.svg"
+                icon: "/assets/icons/weather/heavy_showers-night.svg"
             }
         },
         85: {
             day: {
                 description: "Light Snow Showers",
-                icon: "../../assets/icons/weather/light_snow_showers.svg"
+                icon: "/assets/icons/weather/light_snow_showers.svg"
             },
             night: {
                 description: "Light Snow Showers",
-                icon: "../../assets/icons/weather/light_snow_showers-night.svg"
+                icon: "/assets/icons/weather/light_snow_showers-night.svg"
             }
         },
         86: {
             day: {
                 description: "Snow Showers",
-                icon: "../../assets/icons/weather/snow_showers.svg"
+                icon: "/assets/icons/weather/snow_showers.svg"
             },
             night: {
                 description: "Snow Showers",
-                icon: "../../assets/icons/weather/snow_showers-night.svg"
+                icon: "/assets/icons/weather/snow_showers-night.svg"
             }
         },
         95: {
             day: {
                 description: "Thunderstorm",
-                icon: "../../assets/icons/weather/thunderstorm.svg"
+                icon: "/assets/icons/weather/thunderstorm.svg"
             }
         },
         96: {
             day: {
                 description: "Light Thunderstorms With Hail",
-                icon: "../../assets/icons/weather/light_thunderstorms_with_hail.svg"
+                icon: "/assets/icons/weather/light_thunderstorms_with_hail.svg"
             }
         },
         99: {
             day: {
                 description: "Thunderstorm With Hail",
-                icon: "../../assets/icons/weather/thunderstorm_with_hail.svg"
+                icon: "/assets/icons/weather/thunderstorm_with_hail.svg"
             }
         }
     };
